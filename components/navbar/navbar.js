b539ba52b865954948aee7be59e65429aa01b4c2
StephenLHChan <stephenc.1018@gmail.com>
2022-06-13 15:27:42 -0400

    Add nav-item and nav-menu to navbar

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,10 +1,12 @@
-import { Container, Box, Heading, Flex } from '@chakra-ui/react'
+import { Container, Box, Heading, Flex, Stack } from '@chakra-ui/react'
 
 import Logo from './logo'
 import SourceCodeButton from './source-code-button'
 import ThemeToggleButton from './theme-toggle-button'
+import LinkItem from './link-item'
+import NavMenu from './nav-menu'
 
-const Navbar = () => {
+const Navbar = ({ path }) => {
   return (
     <Box
       position="fixed"
@@ -26,9 +28,27 @@ const Navbar = () => {
             <Logo />
           </Heading>
         </Flex>
+
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, md: 0 }}
+        >
+          <LinkItem href="/projects" path={path}>
+            Projects
+          </LinkItem>
+        </Stack>
+
         <Box flex={1} align="right" alignItems="center" pt="5px" opacity={0.8}>
           <SourceCodeButton />
           <ThemeToggleButton />
+
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <NavMenu />
+          </Box>
         </Box>
       </Container>
     </Box>
