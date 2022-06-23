d88e41a016015bcbdf4dfdb2857694d9871ec621
StephenLHChan <stephenc.1018@gmail.com>
2022-06-23 15:21:41 -0400

    Update crypto-dashboard project page details

--- pages/projects/crypto-dashboard.js
+++ pages/projects/crypto-dashboard.js
@@ -1,7 +1,8 @@
-import { Badge, Container, List, ListItem } from '@chakra-ui/react'
+import { Badge, Container, Icon, Link, List, ListItem } from '@chakra-ui/react'
 import Layout from '../../components/layouts/page'
 import Paragraph from '../../components/paragraph'
 import { Breadcrumb, Tag, ProjectImage } from '../../components/project-item'
+import { IoLogoGithub } from 'react-icons/io5'
 
 const CryptoDashboard = () => {
   return (
@@ -18,10 +19,26 @@ const CryptoDashboard = () => {
         <List ml={4} my={4}>
           <ListItem>
             <Tag>Website</Tag>
+            <Link
+              variant="text-link"
+              href="https://stephen-c-crypto-dashboard.herokuapp.com"
+            >
+              https://stephen-c-crypto-dashboard.herokuapp.com
+            </Link>
           </ListItem>
           <ListItem>
             <Tag>Stacks</Tag>
             <span>Python, pandas, Dash</span>
+          </ListItem>
+          <ListItem>
+            <Tag>Source Code</Tag>
+            <Link
+              href="https://github.com/StephenLHChan/crypto-dashboard"
+              variant="text-link"
+              target="_blank"
+            >
+              Github
+            </Link>
           </ListItem>
         </List>
       </Container>
