7471f3e865e7fe2ad3e9292a7143d18e874c868d
StephenLHChan <stephenc.1018@gmail.com>
2022-07-09 23:04:10 -0400

    Add redirect to LinkedIn and GitHub

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/LinkedIn',
+        destination: 'https://www.linkedin.com/in/stephenlhc',
+        permanent: false
+      },
+      {
+        source: '/GitHub',
+        destination: 'https://github.com/StephenLHChan',
+        permanent: false
+      }
+    ]
+  }
 }
