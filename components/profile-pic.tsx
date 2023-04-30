90a69d7f8f87d02ebb91ba6ea33f58f9a4ba199d
StephenLHChan <stephenc.1018@gmail.com>
2023-04-30 11:03:50 -0400

    fix: simplify profile-pic

--- components/profile-pic.tsx
+++ components/profile-pic.tsx
@@ -1,29 +1,12 @@
-import {
-  Image,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverBody,
-  PopoverArrow
-} from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
 
 const ProfilePic = () => {
   return (
-    <Popover placement="top" isLazy>
-      <PopoverTrigger>
-        <Image
-          src="/images/profile-pic.png"
-          alt="Profile image"
-          borderRadius="full"
-        />
-      </PopoverTrigger>
-      <PopoverContent bg="teal.400" color="white">
-        <PopoverArrow arrowPadding={12} />
-        <PopoverBody fontWeight="semibold">
-          Hello! Welcome to my homepage!
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    <Image
+      src="/images/profile-pic.png"
+      alt="Profile image"
+      borderRadius="full"
+    />
   )
 }
 
