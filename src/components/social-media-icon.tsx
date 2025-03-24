e69d008c87970592a41e13dc4726c5f7cb0e5497
StephenLHChan <stephenc.1018@gmail.com>
2025-03-23 22:08:59 -0400

    feat(shadcn): social-media-icon init

--- src/components/social-media-icon.tsx
+++ src/components/social-media-icon.tsx
@@ -1,10 +1,22 @@
-import { Link, Icon } from '@chakra-ui/react'
-
-const SocialMediaIcon = ({ type, link, icon }) => {
+const SocialMediaIcon = ({
+  type,
+  link,
+  icon: Icon
+}: {
+  type: string
+  link: string
+  icon: React.ElementType
+}) => {
   return (
-    <Link key={type} href={link} variant="icon-link" target="_blank" p={2}>
-      <Icon key={type} as={icon} fontSize="40px" />
-    </Link>
+    <a
+      key={type}
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="p-2 transition-colors hover:text-teal-500 dark:hover:text-[#ff63c3]"
+    >
+      <Icon className="w-10 h-10" />
+    </a>
   )
 }
 
