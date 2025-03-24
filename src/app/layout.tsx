011c6ea29e348b01698bd40c89f129cc46a41e1e
StephenLHChan <stephenc.1018@gmail.com>
2025-03-23 21:29:02 -0400

    feat(shadcn): root layout add missing container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
+import type { Viewport } from 'next'
 import './globals.css'
 
+import { cn } from '@/lib/utils'
+
 import Footer from '@/components/footer'
+import { Navbar } from '@/components/navbar'
 
 export const metadata = {
   title: 'Stephen LH Chan - Homepage',
   description: "Stephen LH Chan's homepage",
-  viewport: 'width=device-width, initial-scale=1',
   icons: {
     icon: '/favicon.ico',
     apple: '/apple-touch-icon.png'
@@ -17,6 +20,11 @@ export const metadata = {
   }
 }
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1
+}
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
@@ -24,8 +32,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <meta name="author" content="Stephen LH Chan" />
       </head>
       <body>
-        {children}
-        <Footer />
+        <main className={cn('pb-8')}>
+          {/* <Navbar path={router.asPath} /> */}
+          <div className="container mx-auto max-w-3xl pt-14">
+            {children}
+            <Footer />
+          </div>
+        </main>
       </body>
     </html>
   )
