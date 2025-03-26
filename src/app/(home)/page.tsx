c01f00a258f72ceb4548f430746515b1012d16cc
StephenLHChan <stephenc.1018@gmail.com>
2025-03-25 21:48:11 -0400

    feat(shadcn): move data to page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,7 +9,105 @@ import Paragraph from '@/components/paragraph'
 import Section from '@/components/section'
 import ProfilePic from '@/components/profile-pic'
 
-import { bioItems, socialMediaList } from '../../../public/data'
+import {
+  IoLogoLinkedin,
+  IoLogoGithub,
+  IoLogoInstagram,
+  IoLogoTwitter,
+  IoLogoFacebook
+} from 'react-icons/io5'
+
+export const socialMediaList = [
+  {
+    type: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/stephenlhc/',
+    icon: IoLogoLinkedin
+  },
+  {
+    type: 'Github',
+    link: 'https://github.com/StephenLHChan/',
+    icon: IoLogoGithub
+  },
+  {
+    type: 'Instagram',
+    link: 'https://www.instagram.com/stephenc.3/',
+    icon: IoLogoInstagram
+  },
+  {
+    type: 'Twitter',
+    link: 'https://twitter.com/StephenLHChan',
+    icon: IoLogoTwitter
+  },
+  {
+    type: 'Facebook',
+    link: 'https://www.facebook.com/StephenLHChan',
+    icon: IoLogoFacebook
+  }
+]
+
+const bioItems = [
+  {
+    id: 1,
+    year: '1992',
+    children: <p>Born in Hong Kong</p>
+  },
+  {
+    id: 2,
+    year: '2016',
+    children: (
+      <p>
+        Completed Bachelor of Engineering in Computer Science in{' '}
+        <a
+          href="https://www.hkust.edu.hk/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-teal-500 dark:hover:text-[#ff63c3] hover:underline"
+        >
+          The Hong Kong University of Science and Technology
+        </a>
+      </p>
+    )
+  },
+  {
+    id: 3,
+    year: '2021',
+    children: <p>Relocated to Toronto, ON</p>
+  },
+  {
+    id: 4,
+    year: '2022',
+    children: (
+      <p>
+        Completed Junior Data Analyst Program offered by{' '}
+        <a
+          href="https://npowercanada.ca"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-teal-500 dark:hover:text-[#ff63c3] hover:underline"
+        >
+          NPower Canada
+        </a>
+      </p>
+    )
+  },
+  {
+    id: 5,
+    year: '2022',
+    children: (
+      <p>
+        Start working as Cloud Monitoring Engineer at{' '}
+        <a
+          href="https://www.bmo.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-teal-500 dark:hover:text-[#ff63c3] hover:underline"
+        >
+          BMO
+        </a>
+      </p>
+    )
+  }
+]
 
 const Home = () => {
   return (
