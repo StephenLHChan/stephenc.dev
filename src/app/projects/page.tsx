c50eb150bf910f4575777d217916bf0c55a2e0be
StephenLHChan <stephenc.1018@gmail.com>
2025-03-25 20:39:59 -0400

    feat(shadcn): projects page init

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from 'next'
+
+import Section from '@/components/section'
+import SectionTitle from '@/components/section-title'
+import { ProjectItem } from '@/components/project-item'
+
+import thumbCrypto from '/public/images/projects/thumbnail_crypto.png'
+import thumbPriceChecker from '/public/images/projects/thumbnail_pricechecker.png'
+
+export const metadata: Metadata = {
+  title: 'Projects - Stephen LH Chan',
+  openGraph: {
+    title: 'Projects - Stephen LH Chan'
+  }
+}
+
+const ProjectPage = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <SectionTitle>Projects</SectionTitle>
+      <Section delay={0.2}>
+        <div className="mt-6 space-y-6">
+          <ProjectItem
+            id="crypto-dashboard"
+            title="Crypto Dashboard"
+            description="A dashboard to display real-time data and graph of cryptocurrency"
+            thumbnail={thumbCrypto}
+          />
+          <ProjectItem
+            id="#"
+            title="Price Checker"
+            description="A web app to check the price of grocery"
+            thumbnail={thumbPriceChecker}
+          />
+        </div>
+      </Section>
+    </div>
+  )
+}
+
+export default ProjectPage
